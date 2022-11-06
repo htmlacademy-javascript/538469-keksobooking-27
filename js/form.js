df0042232c43d5913ddd14da963f5ed721e04a13
AlexandrovAY <AlexandrovAY147@gmail.com>
2022-11-06 10:29:53 +0300

    Исправление 7Ñ.2

--- js/form.js
+++ js/form.js
@@ -1,30 +1,26 @@
 const adForm = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
 
+const toggleElementsState = (elements, state) => {
+  for (const elem of elements) {
+    elem.disabled = state;
+  }
+};
+
 const blockForm = () => {
   adForm.classList.add('ad-form--disabled');
   mapFilters.classList.add('ad-form--disabled');
 
-  for (const elem of adForm.children) {
-    elem.disabled = true;
-  }
-
-  for (const elem of mapFilters.children) {
-    elem.disabled = true;
-  }
+  toggleElementsState(adForm.children, true);
+  toggleElementsState(mapFilters.children, true);
 };
 
 const unblockForm = () => {
   adForm.classList.remove('ad-form--disabled');
   mapFilters.classList.remove('ad-form--disabled');
 
-  for (const elem of adForm.children) {
-    elem.disabled = false;
-  }
-
-  for (const elem of mapFilters.children) {
-    elem.disabled = false;
-  }
+  toggleElementsState(adForm.children, false);
+  toggleElementsState(mapFilters.children, false);
 };
 
 export {blockForm, unblockForm};
